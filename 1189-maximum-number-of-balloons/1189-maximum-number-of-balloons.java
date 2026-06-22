1629880528ab53694c49e61168dea5b1cad98b72
VASUDEV KATARIYA <139060004+vasudev7891@users.noreply.github.com>
2026-06-22 09:34:51 +0530

    Time: 2 ms (96.67%), Space: 43.1 MB (93.6%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+
+        int[] freq = new int[26];
+
+        for (char c : text.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        int b = freq['b' - 'a'];
+        int a = freq['a' - 'a'];
+        int l = freq['l' - 'a'] / 2; 
+        int o = freq['o' - 'a'] / 2; 
+        int n = freq['n' - 'a'];
+
+        return Math.min(Math.min(Math.min(b, a), Math.min(l, o)), n);
+    }
+}
