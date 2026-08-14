ae3b81936bd59739af3a9c08c8d2fa19b2b60e93
VASUDEV KATARIYA <139060004+vasudev7891@users.noreply.github.com>
2026-08-14 17:43:54 +0530

    Time: 1 ms (100%), Space: 43.2 MB (97.93%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -1,24 +1,22 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
-        Map<Character, Integer> map = new HashMap<>();
+        int[] freq = new int[26];
         int l = 0;
         int ans = 0;
-
+        
         for(int r = 0; r < s.length(); r++) {
             char c = s.charAt(r);
-
-            map.put(c, map.getOrDefault(c, 0) + 1);
-
-            while(map.get(c) > 2) {
+            freq[c - 'a']++;
+            
+            while(freq[c - 'a'] > 2) {
                 char d = s.charAt(l);
-
-                map.put(d, map.get(d) - 1);
+                freq[d - 'a']--;
                 l++;
             }
-
+            
             ans = Math.max(ans, r - l + 1);
         }
-
+        
         return ans;
     }
 }
