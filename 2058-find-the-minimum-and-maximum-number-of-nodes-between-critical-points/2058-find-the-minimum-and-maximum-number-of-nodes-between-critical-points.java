3c1eb6215655fc4135a3c21272cef27cd695db09
VASUDEV KATARIYA <139060004+vasudev7891@users.noreply.github.com>
2026-08-31 18:55:10 +0530

    Time: 4 ms (99.87%), Space: 106.2 MB (55.32%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -1,40 +1,37 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        if(head.next.next == null) return new int[]{-1, -1};
+        
+        ListNode prev = head;
+        ListNode curr = head.next;
+        ListNode next = curr.next;
 
-        ListNode prev = head, curr = prev.next, nextN = curr.next;
-        int k = 1;
-        List<Integer> list = new ArrayList<>();
+        int index = 1;
 
-        while(nextN != null) {
-            if(prev.val < curr.val && curr.val > nextN.val) list.add(k);
-            if(prev.val > curr.val && curr.val < nextN.val) list.add(k);
-            k++;
+        int first = -1;
+        int last = -1;
+        int minDistance = Integer.MAX_VALUE;
+
+        while (next != null) {
+
+            if ((curr.val > prev.val && curr.val > next.val) ||
+                (curr.val < prev.val && curr.val < next.val)) {
+
+                if (first == -1) first = index;
+                else minDistance = Math.min(minDistance, index - last);
+
+                last = index;
+            }
 
             prev = curr;
-            curr = nextN;
-            nextN = nextN.next;
+            curr = next;
+            next = next.next;
+            index++;
         }
 
-        if(list.size() < 2) return new int[]{-1, -1};
+        if (first == -1 || first == last) return new int[]{-1, -1};
 
-        int min = Integer.MAX_VALUE;
-        int max = list.get(list.size() - 1) - list.get(0);
+        int maxDistance = last - first;
 
-        for(int i = 1; i < list.size(); i++) {
-            min = Math.min(min, list.get(i) - list.get(i - 1));
-        }
-
-        return new int[]{min, max};
+        return new int[]{minDistance, maxDistance};
     }
 }
