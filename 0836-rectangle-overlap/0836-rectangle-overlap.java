c8d78b49673ab4cadaa7bdc08083f7f0addb49a2
VASUDEV KATARIYA <139060004+vasudev7891@users.noreply.github.com>
2026-09-14 16:56:34 +0530

    Time: 0 ms (100%), Space: 42.5 MB (86.33%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.java
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -1,21 +1,11 @@
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        int x1 = rec1[0], y1 = rec1[1];
-        int x2 = rec1[2], y2 = rec1[3];
-
-        int x3 = rec2[0], y3 = rec2[1];
-        int x4 = rec2[2], y4 = rec2[3];
         
-        if(x3 >= x2) return false;
-        else {
-            if(x4 <= x1) return false;
-        }
-
-        if(y3 >= y2) return false;
-        else {
-            if(y4 <= y1) return false;
-        }
-
+        if (rec1[2] <= rec2[0] ||
+            rec2[2] <= rec1[0] ||
+            rec1[3] <= rec2[1] ||
+            rec2[3] <= rec1[1]) return false;
+        
         return true;
     }
 }
