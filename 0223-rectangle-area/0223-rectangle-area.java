989f19223a1a8b07a6ee235fb78cccc1d11c0d47
VASUDEV KATARIYA <139060004+vasudev7891@users.noreply.github.com>
2026-09-15 16:24:41 +0530

    Time: 2 ms (68.7%), Space: 44.7 MB (13.46%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+       
+        int area1 = (ax2 - ax1) * (ay2 - ay1);
+        int area2 = (bx2 - bx1) * (by2 - by1);
+
+        if(bx1 >= ax2 || 
+           bx2 <= ax1 || 
+           by1 >= ay2 || 
+           by2 <= ay1) return area1 + area2;
+
+        int ox1 = Math.max(ax1, bx1);
+        int oy1 = Math.max(ay1, by1);
+        int ox2 = Math.min(ax2, bx2);
+        int oy2 = Math.min(ay2, by2);
+
+        int area3 = (ox2 - ox1) * (oy2 - oy1);
+
+        return area1 + area2 - area3;
+    }
+}
