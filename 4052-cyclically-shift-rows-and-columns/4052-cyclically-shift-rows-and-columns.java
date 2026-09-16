6d41c7c584660dc9859998b8cac723e3a2159b71
VASUDEV KATARIYA <139060004+vasudev7891@users.noreply.github.com>
2026-09-16 16:49:02 +0530

    Time: 1 ms (100%), Space: 47.5 MB (23.95%) - LeetHub

--- 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.java
+++ 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.java
@@ -1,29 +1,17 @@
 class Solution {
     public int[][] cyclicShift(int n, int[][] grid, int[] rowShift, int[] colShift) {
-        for(int i = 0; i < n; i++) {
-            int times = rowShift[i] % n;
+        int[][] ans = new int[n][n];
 
-            for(int j = 0; j < times; j++) {
-                int first = grid[i][0];
-                for(int k = 1; k < n; k++) {
-                    grid[i][k - 1] = grid[i][k];
-                }
-                grid[i][n - 1] = first;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+
+                int col = (j - rowShift[i] + n) % n;
+                int row = (i - colShift[col] + n) % n;
+
+                ans[row][col] = grid[i][j];
             }
         }
 
-        for(int i = 0; i < n; i++) {
-            int times = colShift[i] % n;
-
-            for(int j = 0; j < times; j++) {
-                int first = grid[0][i];
-                for(int k = 1; k < n; k++) {
-                    grid[k - 1][i] = grid[k][i];
-                }
-                grid[n - 1][i] = first;
-            }
-        }
-
-        return grid;
+        return ans;
     }
 }
