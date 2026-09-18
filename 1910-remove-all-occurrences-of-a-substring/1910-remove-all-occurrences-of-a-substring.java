e39119b95ccfed744589e1e0d9c9bae47fada780
VASUDEV KATARIYA <139060004+vasudev7891@users.noreply.github.com>
2026-09-18 17:54:22 +0530

    Time: 5 ms (41.86%), Space: 43.3 MB (85.68%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -1,28 +1,25 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
         StringBuilder st = new StringBuilder();
-        int top = 0;
         int n = part.length();
 
-        for(char c: s.toCharArray()) {
+        for (char c : s.toCharArray()) {
             st.append(c);
-            top++;
 
-            if(c == part.charAt(n - 1) && top >= n) {
-                int k = top - 1;
-                int x;
-                for(x = n - 1; x >= 0; x--) {
-                    if(part.charAt(x) != st.charAt(k)) break;
+            if (c == part.charAt(n - 1) && st.length() >= n) {
+                int k = st.length() - 1;
+                int i;
+
+                for (i = n - 1; i >= 0; i--) {
+                    if (part.charAt(i) != st.charAt(k)) break;
                     k--;
                 }
 
-                if(x == -1) {
-                    for(int y = 0; y < n; y++) {
-                        st.deleteCharAt(st.length() - 1); 
-                        top--;
+                if (i == -1) {
+                    for (int j = 0; j < n; j++) {
+                        st.deleteCharAt(st.length() - 1);
                     }
                 }
-
             }
         }
 
